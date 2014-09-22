e58b759f316ef1278a6f19faf703d29842769786
CJKu <cjcool.tw@gmail.com>
2014-09-23 00:54:23 +0800

    move out pattern

--- SilkProfiler.py
+++ SilkProfiler.py
@@ -6,46 +6,96 @@ import matplotlib.pyplot as plt
 
 
 class SilkProfiler(object):
-  def __init__(self, name):
-    self.mName = name
-    self.mPattern = re.compile(".*" + self.mName + " *, *([0-9]+) *, *([.0-9]+).*")
+  def __init__(self):
+    self.mXLabel = ""
+    self.mYLabel = ""
+    self.mPattern = None
     self.mMatches = 0       # total amount of matched lines.
     self.mMismatches = 0    # total amount of mismatched lines
     self.mTable = []        # Store peformance data
     self.mSource = None     # Soure log file.
 
-  def Open(self, source):
+  def Open(self, pattern, source):
+    patternFile = None
+
     try:
       self.mSource = open(source, 'r')
+      patternFile = open(pattern, 'r')
+      if False == self._ReadPattern(patternFile):
+        patternFile.close()
+        mSource.close()
+        return False;
+
     except IOError as e:
+      if patternFile != None:
+        patternFile.close()
+        patternFile = None
+
+      if self.mSource != None:
+        self.mSource.close()
+        self.mSource = None
+
       return False
 
     return True
+
+  def _ReadPattern(self, patternFile):
+    """
+    1. Read pattern string
+    2. Read x-axis label string
+    2. Read y-axis label string
+    """
+    for line in patternFile:
+      # Search pattern string in pattern file
+      m = re.search('^pattern[ ]*=[ ]*(.+)', line)
+      if m != None:
+        patternLine = m.group(1)
+        self.mPattern = re.compile(patternLine)
+        break
+
+    patternFile.seek(0)
+    for line in patternFile:
+      # Search x-label string in pattern file
+      m = re.search('^xlabel[ ]*=[ ]*(.+)', line)
+      if m != None:
+        self.mXLabel = m.group(1)
+        break
+
+    patternFile.seek(0)
+    for line in patternFile:
+      # Search y-label string in pattern file
+      m = re.search('^ylabel[ ]*=[ ]*(.+)', line)
+      if m != None:
+        self.mYLabel = m.group(1)
+        break
+
+    return (self.mPattern != None)
 
   def Parse(self):
     for line in self.mSource:
       matched = re.match(self.mPattern, line)
       if matched:
         self.mMatches += 1
-        frame_number = matched.group(1)
-        dist = matched.group(2)
-        entry = (frame_number, dist)
+        x = matched.group('x')
+        y = matched.group('y')
+        entry = (x, y)
         self.mTable.append(entry)
       else:
         self.mMismatches += 1
 
   def Draw(self):
-    dists = []
+    yPlots = []
     for entry in self.mTable:
-      dists.append(float(entry[1]))
+      yPlots.append(float(entry[1]))
 
-    plt.plot(dists)
-    plt.ylabel('touch evet distance')
+    plt.plot(yPlots)
+    plt.xlabel(self.mXLabel)
+    plt.ylabel(self.mYLabel)
     plt.show()
 
   def Print(self):
     for entry in self.mTable:
-      print "frame no. = " + entry[0] + ". dist = " + entry[1]
+      print self.mXLabel + " = " + entry[0] + " / " + self.mYLabel + " = " + entry[1]
 
   def Statistic(self):
     dists = []
--- silk.py
+++ silk.py
@@ -1,18 +1,19 @@
 import SilkProfiler as SP
 import sys
 
-if len(sys.argv) < 2:
-  print "Source log filename is required."
+if len(sys.argv) < 3:
+  print "Positional parameter 1: pattern file."
+  print "Positional parameter 2: Source log filename."
   sys.exit(1)
 
-profiler = SP.SilkProfiler("Silk input resample")
-if False == profiler.Open(sys.argv[1]):
-  print "Source log file is not exist."
+profiler = SP.SilkProfiler()
+if False == profiler.Open(sys.argv[1], sys.argv[2]):
+  print "Pattern file or source log file is not exist."
   sys.exit(1)
 
 profiler.Parse()
 
 # Profile data presentattion
-#profiler.Print()
+profiler.Print()
 profiler.Statistic()
 profiler.Draw()

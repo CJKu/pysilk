5b14ec6736aa8f791de0cacbce00a92180e28183
CJKu <cjcool.tw@gmail.com>
2014-09-24 15:28:39 +0800

    add dirty files

--- testsilkprofiler.py
+++ testsilkprofiler.py
@@ -20,10 +20,10 @@ class TestSilkProfilerFunction(unittest.TestCase):
     self.failIf(True == profiler.Open("", ""))
     self.failIf(True == profiler.Open("/usr/XXX", "/tmp/YYY"))
     self.failIf(True == profiler.Open("", "sample/log.txt"))
-    self.failIf(True == profiler.Open("sample/silk_input_resample.pattern", ""))
+    self.failIf(True == profiler.Open("sample/testpattern_pass.pattern", ""))
 
     # Open correct files
-    self.failIf(False == profiler.Open("sample/silk_input_resample.pattern", "sample/log.txt"))
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/log.txt"))
 
   def testPatternParsing(self):
     profiler = SP.SilkProfiler()
@@ -33,7 +33,20 @@ class TestSilkProfilerFunction(unittest.TestCase):
 
     # Parse valid log files
     self.failIf(False == profiler.Open("sample/testpattern_patternonly.pattern", "sample/log.txt"))
-    self.failIf(False == profiler.Open("sample/silk_input_resample.pattern", "sample/log.txt"))
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/log.txt"))
+
+  def testLogParsing(self):
+    profiler = SP.SilkProfiler()
+
+    # Only one silk ling log in testlog_one.txt
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.txt"))
+    self.failIf(1 != profiler.mParser.mMatches)
+
+    # Open and parse testlog.txt
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog.txt"))
+    total, mean, stdev = profiler.Statistic()
+    self.failIf(10 != total)
+    self.failIf(5.5 != mean)
 
 # Run the unittests
 if __name__ == '__main__':

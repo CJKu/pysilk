2f74c4d73a46b2f432094ac26777d71d91efb580
CJKu <cjcool.tw@gmail.com>
2014-09-27 00:25:23 +0800

    remove create_profiler_in_ipython.py

--- create_profiler_in_ipython.py
+++
@@ -1,6 +0,0 @@
-import silkprofiler as SP
-
-if False == SP.profiler.Open("./sample/silk_input_resample.pattern", "./sample/log.txt"):
-  print "Pattern file or source log file is not exist."
-  sys.exit(1)
-

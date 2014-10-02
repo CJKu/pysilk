7d6d5f728acfa05a47d9f5d9e6c063861511b1b4
CJKu <cjcool.tw@gmail.com>
2014-10-02 12:27:43 +0800

    rename *.txt to *.log

--- testsilkprofiler.py
+++ testsilkprofiler.py
@@ -18,33 +18,33 @@ class TestSilkProfilerFunction(unittest.TestCase):
     profiler = SP.SilkProfiler()
 
     # Parse invalid log files should return False
-    self.failIf(True == profiler.Open("sample/testpattern_empty.pattern", "sample/log.txt"))
-    self.failIf(True == profiler.Open("sample/testpattern_nopattern.pattern", "sample/log.txt"))
+    self.failIf(True == profiler.Open("sample/testpattern_empty.pattern", "sample/log.log"))
+    self.failIf(True == profiler.Open("sample/testpattern_nopattern.pattern", "sample/log.log"))
 
     # Parse valid log files
-    self.failIf(False == profiler.Open("sample/testpattern_patternonly.pattern", "sample/log.txt"))
-    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/log.txt"))
+    self.failIf(False == profiler.Open("sample/testpattern_patternonly.pattern", "sample/log.log"))
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/log.log"))
 
     # Repeat parsing patterns. Make sure context is independent between two parsing
     # Parse testpattern_pass, which has xy label and pattern string, and then parse
     # testpattern_patternonly, which has pattern string only. xy label should be
     # cleared.
     profiler = SP.SilkProfiler()
-    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/log.txt"))
-    self.failIf(False == profiler.Open("sample/testpattern_patternonly.pattern", "sample/log.txt"))
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/log.log"))
+    self.failIf(False == profiler.Open("sample/testpattern_patternonly.pattern", "sample/log.log"))
     self.failIf("" != profiler.mParser.mXLabel)
     self.failIf("" != profiler.mParser.mYLabel)
 
   def testLogParsing(self):
     profiler = SP.SilkProfiler()
 
-    # Only one silk ling log in testlog_one.txt
-    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.txt"))
+    # Only one silk ling log in testlog_one.log
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.log"))
     statistic = profiler.Statistic(False)
     self.failIf(1 != statistic[0])
 
-    # Open and parse testlog.txt
-    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog.txt"))
+    # Open and parse testlog.log
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog.log"))
     total, mean, stdev = profiler.Statistic(False)
     self.failIf(10 != total)
     self.failIf(5.5 != mean)
@@ -52,8 +52,8 @@ class TestSilkProfilerFunction(unittest.TestCase):
     # Repeat parsing logs. Make sure context is independent between two parsing
     # Keep loading testlog_one two times, total samples should not be accumulated.
     profiler = SP.SilkProfiler()
-    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.txt"))
-    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.txt"))
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.log"))
+    self.failIf(False == profiler.Open("sample/testpattern_pass.pattern", "sample/testlog_one.log"))
     statistic = profiler.Statistic(False)
     self.failIf(1 != statistic[0])
 

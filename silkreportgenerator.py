c605d99ed3c8afc7bc024495042caa9039470752
CJKu <cjcool.tw@gmail.com>
2014-10-16 20:04:45 +0800

    check abs file in index file

--- silkreportgenerator.py
+++ silkreportgenerator.py
@@ -24,63 +24,77 @@ class SilkReportGenerator(object):
     '''
     self._Clean()
 
-    # Populate log filenames
+    # Populate log filenames as source of SilkProfiler
     if False == self._PopulateSourceFiles(sourceDir):
       return False
 
-    # Parse and generate figures
+    # Parse and generate figures in outputDir
     if False == self._GenerateReport(patternFile, self.mFileList, outputDir, figSize):
       return False
 
     return True
 
   def _GenerateReport(self, patternFile, files, outputDir, figSize):
+    # Validate patternFile.
     if False == os.path.exists(patternFile) or False == os.path.isfile(patternFile):
       return False
 
-    # For convenience, alwasy destroy output folder in the beginning
+    # For convenience, alwasy destroy output folder, if exists, in the beginning,
+    # so that we have a clean output folder
     if True == os.path.exists(outputDir):
       shutil.rmtree(outputDir)
 
     os.mkdir(outputDir)
 
+    # Use SilkProfiler to generate figure for each source file and put them into
+    # outputDir
     for log in files:
-      if True == os.path.exists(log):
-        profiler = sp.SilkProfiler()
-        if not profiler.Open(patternFile, log):
-          return False
+      if not os.path.exists(log):
+        continue
 
-        # Generate figure
-        (head, tail) = os.path.split(log)
-        intermediate = os.path.join(outputDir, tail)
-        (root, ext) = os.path.splitext(intermediate)
+      profiler = sp.SilkProfiler()
+      if not profiler.Open(patternFile, log):
+        return False
 
-        # XXX: export fileformat config to user?
-        root = root + ".png"
-        if False == profiler.SaveHistogram(sp.Histogram.Line, root, figSize):
-          return False
+      # Generate output fullpath
+      # XXX: export fileformat config to user?
+      (head, tail) = os.path.split(log)
+      (root, ext) = os.path.splitext(os.path.join(outputDir, tail))
+      root = root + ".png"
 
-        # Generate statistic
-        statistic = os.path.join(outputDir, "statistic.txt")
-        with open(statistic, 'a+') as statisticFile:
-          (head, tail) = os.path.split(root)
-          statisticFile.write("[" + tail + "]" + "\n")
-          data = profiler.Statistic(False)
-          for key in data:
-            statisticFile.write(key + " = "+ str(data[key]) + "\n")
+      # Generate Line chart for this log
+      if False == profiler.SaveHistogram(sp.Histogram.Line, root, figSize):
+        return False
+
+      # Generate statistic for this log
+      statistic = os.path.join(outputDir, "statistic.txt")
+      with open(statistic, 'a+') as statisticFile:
+        (head, tail) = os.path.split(root)
+        statisticFile.write("[" + tail + "]" + "\n")
+        data = profiler.Statistic(False)
+        for key in data:
+          statisticFile.write(key + " = "+ str(data[key]) + "\n")
 
     return True
 
   def _PopulateSourceFiles(self, sourceDir):
-    if False == os.path.exists(sourceDir) or False == os.path.isdir(sourceDir):
+    # Validate sourceDir.
+    if not os.path.exists(sourceDir) or not os.path.isdir(sourceDir):
       return False
 
+    # If index file exists, parse that file to gather log file list
+    # Otherwise, collect all *.log files in sourcDir
     index = os.path.join(sourceDir, "index")
     if True == os.path.exists(index):
       with open(index, 'r') as indexFile:
         for line in indexFile:
-          if 0 != len(line.strip()):
-            fullname = os.path.join(sourceDir, line.strip())
+          line = line.strip()
+          if 0 != len(line):
+            if os.path.isabs(line):
+              fullname = line
+            else:
+              fullname = os.path.join(sourceDir, line.strip())
+
             self.mFileList.append(fullname)
     else:
       for (dirpath, dirnames, filenames) in os.walk(sourceDir):
@@ -92,8 +106,8 @@ class SilkReportGenerator(object):
         # Only collect top-level files
         break
 
-      # self.mFileList = [ f for f in os.listdir(sourceDir)
-      #                   if os.path.isfile(os.path.join(sourceDir, f)) ]
+    print "SilkReportGenerator: Gather " + str(len(self.mFileList)) + " files."
+
     return True
 
   def _Clean(self):
